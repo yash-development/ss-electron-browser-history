580fef007aa6e227a63959603bf6f214e06886d5
yashimmunealgorithmics <yash.shukla@immunealgorithmics.com>
2024-11-14 08:03:39 +0530

    SUSEE-162

--- README.md
+++ README.md
@@ -1,1 +1,151 @@
 # ss-electron-browser-history
+
+This module will gather browser history from common internet browsers. Given a time frame. It's not required Node-gyp or any C/C++. It will attempt to read from some common locations.
+
+## Supported operating systems
+
+* Windows
+* Mac
+* Linux (only Firefox and Chrome)
+
+## Supported browsers
+
+| Browser         | Windows | Mac | Linux |
+| --------------- | ---- | --- | ----- |
+| Google Chrome   | ✅    | ✅  | ✅    |
+| Maxthon         | ❌    | ✅  | ❌     |
+| Microsoft Edge  | ✅    | ✅  | ❌     |
+| Mozilla Firefox | ✅    | ✅  | ✅    |
+| Opera           | ✅    | ✅  | ❌     |
+| Seamonkey       | ✅    | ✅  | ❌     |
+| Torch           | ✅    | ❌   | ❌     |
+| Vivaldi         | ✅      | ✅  | ❌     |
+| Brave           | ✅    | ✅  | ❌     |
+| Avast Browser   | ✅    | ✅  | ❌     |
+
+
+# How to Install
+
+> npm install electron-browser-history
+
+**OR**
+
+> yarn install electron-browser-history
+
+# Notes
+
+* You may experience slow downs when dealing with browser that have a larger browser history.
+
+# How to Use
+
+```javascript
+const BrowserHistory = require('electron-browser-history');
+
+//Use In ES6+
+import { 
+  getAllHistory,
+  getFirefoxHistory,
+  getSeaMonkeyHistory,
+  getChromeHistory,
+  getMicrosoftEdge,
+  getBraveHistory,
+  getOperaHistory,
+  getTorchHistory,
+  getMaxthonHistory,
+  getVivaldiHistory
+} from 'electron-browser-history'
+
+
+//Only All Support Browser History
+
+/**
+ * Gets the history for the Specified browsers and time in minutes.
+ * Returns an array of browser records.
+ * @param historyTimeLength | Integer
+ * @returns {Promise<array>}
+ */
+getAllHistory(10).then(function (history) {
+  console.log(history);
+});
+
+
+
+/**
+ * Gets Firefox history
+ * @param historyTimeLength
+ * @returns {Promise<array>}
+ */
+getFirefoxHistory(10).then(function (history) {
+  console.log(history);
+});
+
+
+/**
+ * Gets Seamonkey History
+ * @param historyTimeLength time is in minutes
+ * @returns {Promise<array>}
+ */
+getSeaMonkeyHistory(10).then(function (history) {
+  console.log(history);
+});
+
+
+/**
+ * Gets Chrome History
+ * @param historyTimeLength time is in minutes
+ * @returns {Promise<array>}
+ */
+getChromeHistory(10).then(function (history) {
+  console.log(history);
+});
+
+
+/**
+ * Get Opera History
+ * @param historyTimeLength time is in minutes
+ * @returns {Promise<array>}
+ */
+getOperaHistory(10).then(function (history) {
+  console.log(history);
+});
+
+
+/**
+ * Get Torch History
+ * @param historyTimeLength time is in minutes
+ * @returns {Promise<array>}
+ */
+getTorchHistory(10).then(function (history) {
+  console.log(history);
+});
+
+
+/**
+ * Get Brave History
+ * @param historyTimeLength time is in minutes
+ * @returns {Promise<array>}
+ */
+getBraveHistory(10).then(function (history) {
+  console.log(history);
+});
+
+
+/**
+ * Get Maxthon History
+ * @param historyTimeLength time is in minutes
+ * @returns {Promise<array>}
+ */
+getMaxthonHistory(10).then(function (history) {
+  console.log(history);
+});
+
+/**
+ * Get Vivaldi History
+ * @param historyTimeLength time is in minutes
+ * @returns {Promise<array>}
+ */
+getVivaldiHistory(10).then(function (history) {
+  console.log(history);
+});
+
+```
---
+++ browsers.js
@@ -0,0 +1,119 @@
+const Path =  require('path')
+const fs = require("fs");
+const { setupDefaultPaths: setupPaths } = require('./history_paths');
+
+const CHROME = "Google Chrome",
+    FIREFOX = "Mozilla Firefox",
+    TORCH = "Torch",
+    OPERA = "Opera",
+    SEAMONKEY = "SeaMonkey",
+    VIVALDI = "Vivaldi",
+    MAXTHON = "Maxthon",
+    EDGE = "Microsoft Edge",
+    BRAVE = "Brave",
+    AVAST = "AVAST Browser";
+
+let browserDbLocations = {
+    chrome: "",
+    firefox: "",
+    opera: "",
+    edge: "",
+    torch: "",
+    seamonkey: "",
+    vivaldi: "",
+    maxthon: "",
+    brave: "",
+    avast: ""
+};
+
+
+let defaultPaths = setupPaths();
+
+/**
+ * Find all files recursively in specific folder with specific extension, e.g:
+ * findFilesInDir('./project/src', '.html') ==> ['./project/src/a.html','./project/src/build/index.html']
+ * @param  {String} startPath    Path relative to this file or other file which requires this files
+ * @param  {String} filter       Extension name, e.g: '.html'
+ * @param targetFile
+ * @param depth
+ * @return {Array}               Result files with path string in an array
+ */
+function findFilesInDir(startPath, filter, targetFile, depth = 0) {
+    if(depth === 4){
+        return [];
+    }
+    let results = [];
+    if (!fs.existsSync(startPath)) {
+        //console.log("no dir ", startPath);
+        return results;
+    }
+    let files = fs.readdirSync(startPath);
+    for (let i = 0; i < files.length; i++) {
+        let filename = Path.join(startPath, files[i]);
+        if (!fs.existsSync(filename)) {
+            // console.log('file doesn\'t exist ', startPath);
+            continue;
+        }
+        let stat = fs.lstatSync(filename);
+        if (stat.isDirectory()) {
+            results = results.concat(findFilesInDir(filename, filter, targetFile, depth + 1)); //recurse
+        } else if(filename.endsWith(targetFile) === true) {
+            // console.log('-- found: ', filename);
+            results.push(filename);
+        }
+        /*
+        } else if (filename.indexOf(filter) >= 0 && regExp.test(filename)) {
+            results.push(filename);
+        } else if (filename.endsWith('\\History') === true) {
+            // console.log('-- found: ', filename);
+            results.push(filename);
+        }*/
+    }
+    return results;
+}
+
+
+
+/**
+ * Finds the path to the browsers DB file.
+ * Returns an array of strings, paths, or an empty array
+ * @param path
+ * @param browserName
+ * @returns {Array}
+ */
+function findPaths(path, browserName) {
+    switch (browserName) {
+        case FIREFOX:
+        case SEAMONKEY:
+            return findFilesInDir(path, ".sqlite", Path.sep + 'places.sqlite');
+        case CHROME:
+        case TORCH:
+        case OPERA:
+        case BRAVE:
+        case VIVALDI:
+        case EDGE:
+        case AVAST:
+            return findFilesInDir(path, "History", Path.sep + 'History');
+        case MAXTHON:
+            return findFilesInDir(path, ".dat", Path.sep + 'History.dat');
+        default:
+            return [];
+    }
+}
+
+module.exports = {
+    findPaths,
+    browserDbLocations,
+    defaultPaths,
+    CHROME,
+    FIREFOX,
+    TORCH,
+    OPERA,
+    SEAMONKEY,
+    VIVALDI,
+    MAXTHON,
+    BRAVE,
+    EDGE,
+    AVAST
+};
+
